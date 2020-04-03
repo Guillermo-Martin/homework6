08292589f5e2f698b14b0c557f10779382dc1a1c
gcmartin <gscalica@gmail.com>
2020-04-02 20:43:23 -0700

    Add id=city to input, create section for targeting HTML elements, add basic functionality to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-alert("Connected!");
+// Targeting HTML elements
+var $submit = $('#submit');
+var $city = $('#city');
+
+
+
+
+// Submit button
+$submit.on("click", function(event){
+    event.preventDefault();
+    
+    // get city name
+    var cityName = $city.val();
+    console.log(cityName);
+})
