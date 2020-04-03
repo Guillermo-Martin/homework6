b216f4367b198aea0580b133fe020cd1a70ae2e9
gcmartin <gscalica@gmail.com>
2020-04-02 22:03:18 -0700

    Create variables for api key an query URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,13 @@ $submit.on("click", function(event){
     event.preventDefault();
     
     // get city name
-    var cityName = $city.val();
+    // URI encode:  https://www.sitepoint.com/jquery-decode-url-string/
+    var cityName = encodeURIComponent($city.val().trim());
     console.log(cityName);
-})
+
+    // AJAX request
+    var apiKey = "5dec8c645acb4bf246d950b6137af75e";
+    var queryURL = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    console.log(queryURL);
+    
+});
