e9248d903ae2a7c379bae8b46bf481ae09b360d9
gcmartin <gscalica@gmail.com>
2020-04-03 17:27:51 -0700

    Create variables to hold weather data and create AJAX request for UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ $submit.on("click", function(event){
     var cityName = encodeURIComponent($city.val().trim());
     console.log(cityName);
 
-    // AJAX request
+    // AJAX request for main weather
     var apiKey = "5dec8c645acb4bf246d950b6137af75e";
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
     console.log(queryURL);
@@ -23,7 +23,37 @@ $submit.on("click", function(event){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        // console.log(response);
+
+        // city:  console.log(response.name);
+        var searchedCity = response.name;
+        // current temp:  console.log(response.main.temp);
+        var currentTemp = response.main.temp;
+        // current humidity:  console.log(response.main.humidity);
+        var currentHumidity = response.main.humidity;
+        // current wind speed console.log(response.wind.speed);
+        var currentWind = response.wind.speed;
+        // longitude for uv index:  console.log(response.coord.lon);
+        var longitude = response.coord.lon;
+        // latitude for uv index:  console.log(response.coord.lat);
+        var latitude = response.coord.lat;
+        
+
+
+        // AJAX request for UV index
+        var uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${latitude}&lon=${longitude}`;
+
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(uvResponse){
+            // console.log(uvResponse);
+
+            // uv index:  console.log(uvResponse.value)
+            var uvIndex = uvResponse.value;
+
+        });
+    
     });
     
 });
